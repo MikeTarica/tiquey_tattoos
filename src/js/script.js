e2f3aa866626aa9bba4107fbe68477aa66245067
mike Tarica <mike.tarica@me.com>
2023-04-19 11:22:15 +0200

    html fix

--- src/js/script.js
+++ src/js/script.js
@@ -3,17 +3,17 @@
 var slidePosition = 0;
 SlideShow(slidePosition);
 
-// forward/Back controls
+  // forward/Back controls
 function plusSlides(n) {
   SlideShow(slidePosition += n);
 }
 
-//  images controls
+  //  images controls
 function currentSlide(n) {
   SlideShow(slidePosition = n);
 }
 
-function SlideShow() {
+function SlideShow(n) {
   var i;
   var slides = document.getElementsByClassName("Containers");
   for (i = 0; i < slides.length; i++) {
@@ -22,6 +22,6 @@ function SlideShow() {
   slidePosition++;
   if (slidePosition > slides.length) {slidePosition = 1}
   slides[slidePosition-1].style.display = "block";
-  setTimeout(SlideShow, 7000); // Change image every 7 seconds
+  setTimeout(SlideShow, 3500); // Change image every 7 seconds
 } 
 
